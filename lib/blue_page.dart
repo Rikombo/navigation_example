9358f91feb93a102e39f7d4a6aefd4fe6b824f53
Valentins Stepanovs <valentin.stepano@gmail.com>
2023-06-22 17:03:47 +0300

    FAVS-22: Now red color page update it to color from second page

--- lib/blue_page.dart
+++ lib/blue_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class BluePage extends StatelessWidget {
   const BluePage({super.key, required this.color, required this.onClose});
 
-  final Color? color;
+  final Color color;
   final void Function()? onClose;
 
   @override
@@ -11,13 +11,13 @@ class BluePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Color',
+          'Color Page',
         ),
       ),
       body: GestureDetector(
         onTap: () => _closePage(context),
         child: Container(
-          color: color ?? Colors.purple,
+          color: color,
         ),
       ),
     );
@@ -25,6 +25,6 @@ class BluePage extends StatelessWidget {
 
   void _closePage(BuildContext context) {
     onClose?.call();
-    Navigator.of(context).pop(Colors.yellow);
+    Navigator.of(context).pop(color);
   }
 }
